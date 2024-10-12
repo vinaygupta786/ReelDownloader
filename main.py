c1998a8228edfdfacfedd324d43906c6f4f83828
Djkatty <61006486+vinaygupta786@users.noreply.github.com>
2024-10-12 21:10:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ app = Flask(__name__)
 
 def download_video(video_url):
     ydl_opts = {
-    'format': 'best',
-    'outtmpl': 'downloads/%(title)s.%(ext)s',
-    'nocheckcertificate': True,  # Skips SSL certificate checks
-    'extractor_args': {
-        'youtube': {
-            'skip_dash_manifest': True,  # Avoids issues with DASH streams
+        'format': 'best',
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'nocheckcertificate': True,  # Skips SSL certificate checks
+        'proxy': 'http://20.204.214.79:3129',  # Add your proxy here
+        'extractor_args': {
+            'youtube': {
+                'skip_dash_manifest': True,  # Avoids issues with DASH streams
+            },
         },
-    },
-    'headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        'headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        }
     }
-}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(video_url, download=True)
